7a9bdbe057994cebb7dae0d83c1ef6504edb6d99
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-03-08 22:05:12 +0100

    Upgrade dependency spring boot

--- spring-boot/src/main/java/io/github/project/openubl/quickstart/xbuilder/springboot/Application.java
+++ spring-boot/src/main/java/io/github/project/openubl/quickstart/xbuilder/springboot/Application.java
@@ -18,7 +18,10 @@ package io.github.project.openubl.quickstart.xbuilder.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
+@ComponentScan("io.github.project.openubl.spring.xsender.runtime")
 @SpringBootApplication
 public class Application {
 
